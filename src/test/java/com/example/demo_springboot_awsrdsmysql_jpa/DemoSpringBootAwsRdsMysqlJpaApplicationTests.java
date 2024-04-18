d8b89790e22b8bd5dc38e3472125df50a02e3e4c
Zhen Yang <49733188+gitzhen0@users.noreply.github.com>
2024-04-18 18:16:30 -0400

    Initial commit

---
+++ src/test/java/com/example/demo_springboot_awsrdsmysql_jpa/DemoSpringBootAwsRdsMysqlJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_springboot_awsrdsmysql_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootAwsRdsMysqlJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
